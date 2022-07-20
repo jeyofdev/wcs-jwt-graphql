0c97f3d6dddd641566e331df767de25cc9eee743
jeyofdev <jgregoire.pro@gmail.com>
2022-07-20 09:14:46 +0200

    add credential option in ApolloClient instance

--- client/src/config/apollo.ts
+++ client/src/config/apollo.ts
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: process.env.REACT_APP_GRAPHQL_URL,
+    credentials: 'include', // PENSEZ A RAJOUTER CA!!!!
     cache: new InMemoryCache({
         addTypename: false, // remove key __typename in query results
     }),
